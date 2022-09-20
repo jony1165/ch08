7d0b564e8327e482a661b64d3621584647e760d8
jony1165 <113482612+jony1165@users.noreply.github.com>
2022-09-21 01:23:24 +0800

    Update class05.java

--- class05.java
+++ class05.java
@@ -1,5 +1,5 @@
 package hw05;
-class CBox        // �w�qCRectangle���O
+class CBox
 {
 	int length;
 	int width;
@@ -12,15 +12,15 @@ class CBox        // �w�qCRectangle���O
 	   {
 		 System.out.println(length*width*2+height*length*2+width*height*2);      // 傳回箱子的表面積
 	   }
-	void showData()   // 定義成員函數volume(), 用來計算體積
+	void showData()   
 	   {
 		 System.out.println("length="+length);
 	     System.out.println("width="+width);
 	     System.out.println("height="+height);
 	   }
-	void showall()       // show_all() method,同時顯示出半徑與圓面積
+	void showall()       
 	  {
-	     showData();          // 於類別內呼叫show_area() method
+	     showData();          
 	     volume();
 	     surfaceArea();
 	  }
